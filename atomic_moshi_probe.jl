a9f2bb6eb3b2b419e4e4cfe796df015c15c92a0c
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-09 10:16:16 +0200

    dev: probe atomic cache holder through Moshi

--- atomic_moshi_probe.jl
+++ atomic_moshi_probe.jl
@@ -1,15 +1,22 @@
 using Moshi.Data: @data
 
+mutable struct AtomicCache{T}
+    @atomic value::Union{Nothing, T}
+    AtomicCache{T}() where {T} = new{T}(nothing)
+end
+
 @data mutable Probe{T} begin
     struct Node
-        @atomic value::Union{Nothing, T}
+        const cache::AtomicCache{T}
     end
 end
 
-x = Probe.Node{Vector{Int}}(nothing)
-@assert (@atomic :acquire x.value) === nothing
+x = Probe.Node{Vector{Int}}(AtomicCache{Vector{Int}}())
+storage = Moshi.Data.variant_storage(x)
+cache = getfield(storage, :cache)
+@assert (@atomic :acquire cache.value) === nothing
 candidate = [1, 2, 3]
-result = @atomicreplace :release :monotonic x.value nothing => candidate
+result = @atomicreplace :acquire_release :acquire cache.value nothing => candidate
 @assert result.success
-@assert (@atomic :acquire x.value) === candidate
-println("atomic Moshi field works")
+@assert (@atomic :acquire cache.value) === candidate
+println("atomic cache holder works through Moshi")
